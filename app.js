e2c9024c1b10ee2103f94b717e2ea594b91217f4
Muhammad-Asif-Ansari <muhammadasifansari101@gmail.com>
2024-10-03 21:06:08 +0500

    Update assign JavaScript
    Update assign JavaScript

---
+++ app.js
@@ -0,0 +1,136 @@
+
+// Assignment Chapter 17 to 20:
+
+// Question No 01:
+//  Declare and initialize an empty multidimensional array.
+// (Array of arrays)
+
+// var arr1 = [[1,2,3,4,5],["kashif","asif","kaif"], [true, false]];
+
+// console.log(arr1.join(""));
+
+// Question No 02:
+//  Declare and initialize a multidimensional array 
+// representing the following matrix:
+// var arrayMatrix = [
+//     [0,1,2,3],
+//     [1,0,1,2],
+//     [2,1,0,2]
+// ];
+// console.log(arrayMatrix);
+
+// Question No 03:
+// Write a program to print numeric counting from 1 to 10
+// for(var  i = 1; i <= 10; i++){
+//      console.log(i);
+// }
+
+
+
+
+// Question No 04:
+// Write a program to print multiplication table of any 
+// number using for loop.
+//  var table = prompt("Enter a Number");
+// if(table == ""){
+//        table = 5
+// }
+// for(var i = 1; i <= 100 ; i++){
+//        document.write(table + " * " + i + " = " + table * i  + "<br>"); 
+// }
+
+// Question No 05:
+// Write a program to print items of the following array
+// using for loop:
+// fruits = [“apple”, “banana”, “mango”, “orange”,
+// “strawberry”]
+
+
+// var fruits =  ["apple", "banana", "mango", "orange", "strawberry"];
+// for(var i = 0; i < fruits.length; i++){
+//     console.log("Elements at index 0 is "+ fruits[0]);
+//     console.log("Elements at index 1 is "+ fruits[1]);
+//     console.log("Elements at index 2 is "+ fruits[2]);
+//     console.log("Elements at index 3 is "+ fruits[3]);
+//     console.log("Elements at index 4 is "+ fruits[4]);
+//     }
+
+
+
+// Question No 06:
+// var number = 1;
+// for(var i = number; i <=  20; i++){
+//     console.log(i);
+// }
+
+// for(var i = 1; i < 20; i++){
+//     console.log("2k",20-i);
+// }
+
+
+
+
+// Question No 07:
+// You have an array
+// A = [“cake”, “apple pie”, “cookie”, “chips”, “patties”]
+// Write a program to enable “search by user input” in an
+// array.
+// After searching, prompt the user whether the given item is
+// found in the list or not. Example:
+
+// var A = ["cake","apple pie","cookie","chips","patties"];
+// var userInput = prompt("Enter search  item");
+
+// if(A.indexOf(userInput) !== -1 ){
+//     console.log(userInput + " is availabe in our bakery");
+// }
+// else{
+//     console.log(userInput + " We are Sorry.is not available in our bakery ");
+
+// }
+
+
+
+// Question No 08:
+// Write a program to identify the largest number in the
+// given array.
+// A = [24, 53, 78, 91, 12].
+
+// var A = [24, 53, 78, 91, 12];
+// var max = A[0];
+// for (var i = 1; i < A.length; i++){
+//     if (A[i] > max){
+//         max = A[i];
+//     }
+// }
+// console.log("Largest number is " + max);
+
+// Question No 09:
+// Write a program to identify the smallest number in the 
+// given array.
+// A = [24, 53, 78, 91, 12];
+// var min = A[0];
+// for(var i = 0; i < A.length; i++){
+//     if(A[i] < min){
+//         min = A[i];
+//     }
+// }
+// console.log("Smallest number is " + min);
+
+
+// Question No 10:
+// Write a program to print multiples of 5 ranging 1 to
+// 100
+
+//  var  i = 1;
+//  while (i <= 100) {
+//     if (i % 5 === 0) {
+//         console.log(i);
+//         }
+//         i++;
+//         }
+
+
+
+
+
